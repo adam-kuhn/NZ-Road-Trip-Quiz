c215ee545efc469027b153fe3f06bd72a855074e
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-06 11:40:14 +1200

    able to use params but not in the class func

--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
-class Quiz extends React.Component {
-  componentDidMount () {
-  // make api call with params, to get questions
-  }
+// class Quiz extends React.Component {
+//   constructor ({match}) {
+//     super({match})
+//   }
+//   componentDidMount () {
+//   // make api call with params, to get questions
+//   }
 
-  render () {
-    return (
-      <h1>This is a quiz</h1>
-    )
-  }
+//   render ({match}) {
+//     const type = match.params.topic
+//     return (
+//       <h1>This is a {type} quiz</h1>
+//     )
+//   }
+// }
+function Quiz ({match}) {
+  const topic = match.params.topic
+  return (
+    <h1>This is a {topic} quiz </h1>
+  )
 }
 
 export default Quiz
