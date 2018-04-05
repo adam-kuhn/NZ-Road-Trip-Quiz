be71ae22ca8da5caa4b49151de53ee956d9939cc
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-06 11:45:23 +1200

    capitlize first letter

--- client/components/QuizSideBar.jsx
+++ client/components/QuizSideBar.jsx
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom'
 function QuizSideBar () {
   return (
     <ul>
-      <li><Link to={'/quiz/road'}>Road Quiz</Link></li>
-      <li><Link to ={'/quiz/trekking'}>Trekking Quiz</Link></li>
-      <li><Link to ={'/quiz/picture'}>Picture Quiz</Link></li>
-      <li><Link to ={'/quiz/rapid'}>Rapid Fire</Link></li>
+      <li><Link to={'/quiz/Road'}>Road Quiz</Link></li>
+      <li><Link to ={'/quiz/Trekking'}>Trekking Quiz</Link></li>
+      <li><Link to ={'/quiz/Picture'}>Picture Quiz</Link></li>
+      <li><Link to ={'/quiz/Rapid'}>Rapid Fire</Link></li>
     </ul>
   )
 }
