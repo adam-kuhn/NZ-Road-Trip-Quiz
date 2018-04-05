e8e9c64185108a22a9126d8bf91e0aa2684d7221
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-06 11:47:00 +1200

    basic desc on home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,6 +6,7 @@ function Home () {
   return (
     <div>
       <h1>NZ Road Trip Quiz!</h1>
+      <p>Select a quiz and have some fun finding out about my New Zealand Road Trip</p>
       <QuizSideBar />
     </div>
   )
