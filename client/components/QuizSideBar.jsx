c19d62c8be05f27a07205eaf4bc7c13f9ff59080
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-06 11:43:37 +1200

    links quiz topic set up

--- client/components/QuizSideBar.jsx
+++ client/components/QuizSideBar.jsx
@@ -5,9 +5,9 @@ function QuizSideBar () {
   return (
     <ul>
       <li><Link to={'/quiz/road'}>Road Quiz</Link></li>
-      <li>Trekking Quiz</li>
-      <li>Picture Quiz</li>
-      <li>Rapid Fire </li>
+      <li><Link to ={'/quiz/trekking'}>Trekking Quiz</Link></li>
+      <li><Link to ={'/quiz/picture'}>Picture Quiz</Link></li>
+      <li><Link to ={'/quiz/rapid'}>Rapid Fire</Link></li>
     </ul>
   )
 }
