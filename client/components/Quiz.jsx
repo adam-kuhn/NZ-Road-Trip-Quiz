08403d37389f0c6604cdfac1ac9c2cbff64d6aa4
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-06 16:26:42 +1200

    use match with class...not sure if class or fun is best atm

--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 
 import Questions from './Questions'
 
-function Quiz ({match}) {
-  const topic = match.params.topic
-  return (
-    <div>
-      <h1>This is a {topic} quiz </h1>
-      <Questions />
-    </div>
-  )
+// function Quiz ({match}) {
+//   const topic = match.params.topic
+//   return (
+//     <div>
+//       <h1>This is a {topic} quiz </h1>
+//       <Questions />
+//     </div>
+//   )
+// }
+
+class Quiz extends React.Component {
+  render () {
+    return (
+      <h1>{this.props.match.params.topic} quiz</h1>
+    )
+  }
 }
 
 export default Quiz
