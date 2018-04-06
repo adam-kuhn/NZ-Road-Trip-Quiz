5a5b1d6555e5fc165637ef05df3d597baa85ef92
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-06 12:01:02 +1200

    place holder question and answer componented

--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -1,24 +1,14 @@
 import React from 'react'
 
-// class Quiz extends React.Component {
-//   constructor ({match}) {
-//     super({match})
-//   }
-//   componentDidMount () {
-//   // make api call with params, to get questions
-//   }
+import Questions from './Questions'
 
-//   render ({match}) {
-//     const type = match.params.topic
-//     return (
-//       <h1>This is a {type} quiz</h1>
-//     )
-//   }
-// }
 function Quiz ({match}) {
   const topic = match.params.topic
   return (
-    <h1>This is a {topic} quiz </h1>
+    <div>
+      <h1>This is a {topic} quiz </h1>
+      <Questions />
+    </div>
   )
 }
 
