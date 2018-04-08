5ef58c41e1bc78a8577af6665d13b8c444c9c90b
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-08 18:17:30 +1200

    linting

--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -2,16 +2,6 @@ import React from 'react'
 
 import Questions from './Questions'
 
-// function Quiz ({match}) {
-//   const topic = match.params.topic
-//   return (
-//     <div>
-//       <h1>This is a {topic} quiz </h1>
-//       <Questions />
-//     </div>
-//   )
-// }
-
 class Quiz extends React.Component {
   render () {
     return (
