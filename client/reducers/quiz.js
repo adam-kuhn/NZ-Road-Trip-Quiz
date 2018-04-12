5c99bf6b7f89905f000bc22fd35e61d6e72fd65e
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-12 16:42:24 +1200

    store quiz in store

--- client/reducers/quiz.js
+++ client/reducers/quiz.js
@@ -1,9 +1,18 @@
+import {GOT_QUIZ} from '../actions'
+
 const initialState = {
   initial: 'state'
 }
 
 function quiz (state = initialState, action) {
-  return initialState
+  switch (action.type) {
+    case (GOT_QUIZ): {
+      return action.questions
+    }
+    default: {
+      return initialState
+    }
+  }
 }
 
 export default quiz
