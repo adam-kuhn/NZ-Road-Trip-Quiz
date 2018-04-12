39b48bf29b3b1d58b096ef529305af15eac22d99
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-13 11:48:01 +1200

    can only select one answer

--- client/components/Responses.jsx
+++ client/components/Responses.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 class Answers extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      selected: '',
+      correct: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick (evt) {
+    const correct = evt.target.correct
+    this.setState({
+      selected: Number(evt.target.value),
+      correct: correct
+    })
+  }
+
   render () {
     return (
       <div>
         {this.props.answers.map(answer => {
           return (
             <label key={answer.id}>{answer.response}
-              <input type='radio' value={answer.correct} />
+              <input type='radio' onChange={this.handleClick} value={answer.id}
+                checked={this.state.selected === answer.id}
+                data-correct={answer.correct} />
             </label>
           )
         })}
