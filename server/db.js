35c02ed9689f69bd0429804e6af280eecb75e331
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-12 13:40:13 +1200

    assemble questions and response into one object

--- server/db.js
+++ server/db.js
@@ -2,6 +2,8 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')[environment]
 const connection = require('knex')(config)
 
+const util = require('util')
+
 function getQuiz (topic, testDb) {
   const db = testDb || connection
   console.log('db', topic)
@@ -21,11 +23,12 @@ function getQuiz (topic, testDb) {
       return db('questions')
         .join('answers', 'questions.id', 'answers.questions_id')
         .where('questions.topic', topic)
-        .select('answers.response', 'answers.correct', 'answers.description', 'answers.questions_id')
+        .select('answers.id', 'answers.response', 'answers.correct', 'answers.description', 'answers.questions_id')
         .then(result => {
           console.log(result)
           const answers = result.map(response => {
             return {
+              id: response.id,
               questionId: response.questions_id,
               response: response.response,
               correct: response.correct,
@@ -33,8 +36,17 @@ function getQuiz (topic, testDb) {
             }
           })
           console.log(answers)
+          for (let i in questions) {
+            let answersToQuestion = answers.filter(answer => {
+              return questions[i].id === answer.questionId
+            })
+            for (let j in answersToQuestion) {
+              delete answersToQuestion[j].questionId
+            }
+            questions[i].responses = answersToQuestion
+          }
+          console.log(util.inspect(questions, false, null))
         })
-     
     })
     .catch(err => {
       console.error(err)
