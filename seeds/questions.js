104abf95db33e19220d1b7c3fce606b0e95cdbe7
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-19 22:08:51 +1200

    seed trekking quiz;

--- seeds/questions.js
+++ seeds/questions.js
@@ -16,7 +16,15 @@ exports.seed = function (knex, Promise) {
         {id: 10, question: 'How many times did I press skip on the playlist', style: 'multi', topic: 'road'},
         {id: 11, question: 'How many speeding tickets did I get?', style: 'multi', topic: 'road'},
         {id: 12, question: 'How many pictures did I take?', style: 'multi', topic: 'picture'},
-        {id: 13, question: 'How many selfies did I take?', style: 'multi', topic: 'picture'}
+        {id: 13, question: 'How many selfies did I take?', style: 'multi', topic: 'picture'},
+        {id: 14, question: 'How many km did I travel on my longest trek?', style: 'multi', topic: 'trekking'},
+        {id: 15, question: 'How many total km did I travel in my 8 days of trekking?', style: 'multi', topic: 'trekking'},
+        {id: 16, question: 'In total how many vertical meters did I climb', style: 'multi', topic: 'trekking'},
+        {id: 17, question: 'What was the most vertical I climbed in one day?', style: 'multi', topic: 'trekking'},
+        {id: 18, question: 'What was my favorite hike?', style: 'multi', topic: 'trekking'},
+        {id: 19, question: 'I had to doo the deed on a trek. What did I have to use for TP?', style: 'multi', topic: 'trekking'},
+        {id: 20, question: 'What was the dumbest thing I did on one of my treks', style: 'multi', topic: 'trekking'},
+        {id: 21, question: 'How many blisters did I end up wtih', style: 'multi', topic: 'trekking'}
       ])
     })
 }
