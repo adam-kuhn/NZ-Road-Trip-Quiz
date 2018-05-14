6256c0ad38076a876c46147cdb8ad8f32c4a1dff
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-13 19:46:25 -0600

    remove test text from desc component

--- client/components/Description.jsx
+++ client/components/Description.jsx
@@ -4,7 +4,6 @@ function Description (props) {
   return (
     <div>
       {props.img !== null && <img src={props.img} />}
-      {props.img === null && <p>no image</p>}
       <p>{props.text}</p>
     </div>
 
