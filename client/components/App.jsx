6a71178ca1bbb58b6dc8b13f7d32d53a93e64511
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-17 20:03:54 -0600

    update stepper color

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import {cyan600} from 'material-ui/styles/colors'
 
 import Home from './Home'
 import Quiz from './Quiz'
 const App = () => {
+  const muiTheme = getMuiTheme({
+    stepper: {
+      iconColor: cyan600
+    }
+  })
   return (
     <Router>
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme} >
         <div>
           <Route path='/' component={Home} />
           <Route path='/quiz/:topic' component={Quiz} />
