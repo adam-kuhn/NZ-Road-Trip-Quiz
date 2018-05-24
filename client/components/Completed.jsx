9552dec68d4c0e1308550ee093fe9e3d7c40e30b
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-24 13:05:06 +1200

    display score on complete component

--- client/components/Completed.jsx
+++ client/components/Completed.jsx
@@ -20,11 +20,21 @@ class Completed extends React.Component {
     return (
       <div>
         <p>quiz completed</p>
+        <p>You scored {this.props[this.props.topic]}/10</p>
         <button className='home-button' type='button' onClick={this.handleClick}>
           <HomeIcon /></button>
       </div>
     )
   }
 }
+function mapStateToProps (state) {
+  return {
+    road: state.score.road,
+    trekking: state.score.trekking,
+    picture: state.score.picture,
+    fun: state.score.fun,
+    speed: state.score.speed
+  }
+}
 
-export default connect()(withRouter(Completed))
+export default connect(mapStateToProps)(withRouter(Completed))
