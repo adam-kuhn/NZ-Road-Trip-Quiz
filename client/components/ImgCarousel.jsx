6d9749a34d488f17dfeb38fae7a0bcb62f6fea7b
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-29 08:56:52 +1200

    carousel to map over images

--- client/components/ImgCarousel.jsx
+++ client/components/ImgCarousel.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import {Carousel} from 'react-responsive-carousel'
 
+const images = ['/images/questions/alluvial_fan.jpg', '/images/questions/east-coast.jpg']
 class ImgCarousel extends React.Component {
   render () {
     return (
       <Carousel autoPlay>
-        <div>
-          <img src='/images/questions/alluvial_fan.jpg' />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src='/images/questions/east-coast.jpg' />
-          <p className="legend">Legend 2</p>
-        </div>
+        {images.map((image, idx) => {
+          return (
+            <div key={idx}>
+              <img src={image} />
+            </div>
+          )
+        })}
       </Carousel>
     )
   }
