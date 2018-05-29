4ea2b140e19ebadaca76c3204e17de9f4eeac067
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-29 15:43:10 +1200

    create lib file for carousel images

--- client/components/ImgCarousel.jsx
+++ client/components/ImgCarousel.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Carousel} from 'react-responsive-carousel'
 
-const images = ['/images/questions/alluvial_fan.jpg', '/images/questions/east-coast.jpg']
+import images from '../lib/carouselImg'
+
 class ImgCarousel extends React.Component {
   render () {
     return (
